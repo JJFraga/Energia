5c4bb88f7a28e29002d6875cefb1f165995910f1
JJ <juanfragamadrid@gmail.com>
2022-04-19 12:55:19 +0200

    Nuevas clases

--- conversor/src/main/java/desalacion/Desalador.java
+++ conversor/src/main/java/desalacion/Desalador.java
@@ -2,8 +2,32 @@ package desalacion;
 
 public class Desalador
 {
-    public  Desalador()
-    {
+    private String _id = "";
+    private int _version = 0;
 
+    public Desalador(String id, int version)
+    {
+        this._id = id;
+        this._version=version;
+    }
+
+    public String getid()
+    {
+        return this._id;
+    }
+
+    public void set_id(String id)
+    {
+        this._id = id;
+    }
+
+    public int get_version()
+    {
+        return this._version;
+    }
+
+    public void set_version(int version)
+    {
+        this._version = version;
     }
 }
--- plantaEolica/src/main/java/es/aplideka/energia/Instructor.java
+++ plantaEolica/src/main/java/es/aplideka/energia/Instructor.java
@@ -4,6 +4,42 @@ public class Instructor
 {
     private int jj = 0;
     private int jsj = 0;
-
     private int aa = 0;
+
+    public Instructor(int jj, int jsj, int aa)
+    {
+        this.jj = jj;
+        this.jsj = jsj;
+        this.aa = aa;
+    }
+
+    public int getJj()
+    {
+        return jj;
+    }
+
+    public void setJj(int jj)
+    {
+        this.jj = jj;
+    }
+
+    public int getJsj()
+    {
+        return jsj;
+    }
+
+    public void setJsj(int jsj)
+    {
+        this.jsj = jsj;
+    }
+
+    public int getAa()
+    {
+        return aa;
+    }
+
+    public void setAa(int aa)
+    {
+        this.aa = aa;
+    }
 }
